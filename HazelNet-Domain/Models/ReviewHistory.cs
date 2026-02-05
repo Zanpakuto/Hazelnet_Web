0cb9cd800d002da45ccde21e2837c6bf5de352fe
Vhon Michael <vhontabalan@gmail.com>
2026-02-06 01:52:28 +0800

    feat: change ICollection to list

--- HazelNet-Domain/Models/ReviewHistory.cs
+++ HazelNet-Domain/Models/ReviewHistory.cs
@@ -3,7 +3,7 @@ namespace HazelNet_Domain.Models;
 public class ReviewHistory
 {
     public int CardId { get; private set; }
-    public ICollection<ReviewLog> ReviewLogs { get; private set; } = new List<ReviewLog>();
+    public List<ReviewLog> ReviewLogs { get; private set; } = new List<ReviewLog>();
 
     public ReviewHistory(int cardId)
     {
