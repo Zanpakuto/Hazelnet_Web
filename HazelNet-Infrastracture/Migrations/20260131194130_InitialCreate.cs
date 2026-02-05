9bbc5029ee3498054b6d2d554171299644bb1522
Vhon Michael <vhontabalan@gmail.com>
2026-02-06 01:56:28 +0800

    feat: adding database migrations

---
+++ HazelNet-Infrastracture/Migrations/20260131194130_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HazelNet_Infrastractire.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Decks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DeckName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastAcess = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Decks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FrontOfCard = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BackOfCard = table.Column<string>(type: "text", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeckId = table.Column<int>(type: "integer", nullable: false),
+                    Due = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Stability = table.Column<double>(type: "double precision", nullable: false),
+                    Difficulty = table.Column<double>(type: "double precision", nullable: false),
+                    ElapsedDays = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    ScheduledDays = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Reps = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Lapses = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    State = table.Column<short>(type: "smallint", nullable: false),
+                    LastReview = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_Decks_DeckId",
+                        column: x => x.DeckId,
+                        principalTable: "Decks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_DeckId",
+                table: "Cards",
+                column: "DeckId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Decks");
+        }
+    }
+}
