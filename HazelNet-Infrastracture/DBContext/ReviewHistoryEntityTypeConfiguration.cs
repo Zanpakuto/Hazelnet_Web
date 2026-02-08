7e40649df23d2046b02a3bfa69b4e9cc4a014393
Vhon Michael <vhontabalan@gmail.com>
2026-02-08 23:51:58 +0800

    feat: add entity type configurations for EF Core mappings

---
+++ HazelNet-Infrastracture/DBContext/ReviewHistoryEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using HazelNet_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HazelNet_Infrastracture.DBContext;
+
+public class ReviewHistoryEntityTypeConfiguration:  IEntityTypeConfiguration<ReviewHistory>
+{
+    public void Configure(EntityTypeBuilder<ReviewHistory> builder)
+    {
+        builder.HasKey(h => h.Id);
+        builder.Property(h => h.Id).ValueGeneratedNever();
+
+        builder.HasMany(h => h.ReviewLogs)
+            .WithOne(l => l.ReviewHistory)
+            .HasForeignKey(l => l.ReviewHistoryId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ HazelNet-Infrastracture/DBContext/ReviewLogEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using HazelNet_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HazelNet_Infrastracture.DBContext;
+
+public class ReviewLogEntityTypeConfiguration :  IEntityTypeConfiguration<ReviewLog>
+{
+    public void Configure(EntityTypeBuilder<ReviewLog> builder)
+    {
+        builder.HasKey(r => r.Id);
+        builder.Property(r => r.Id).ValueGeneratedNever();
+
+        builder.HasOne(r => r.ReviewHistory)
+            .WithMany(l => l.ReviewLogs)
+            .HasForeignKey(r => r.ReviewHistoryId);
+    }
+}
