c4b1ab6f17405d51407dcf34ac1fc8b1cb72d01b
Vhon Michael <vhontabalan@gmail.com>
2026-02-24 00:18:06 +0800

    chore: new db migration

---
+++ HazelNet-Infrastracture/Migrations/20260223161440_AddingUsers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HazelNet_Infrastractire.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
