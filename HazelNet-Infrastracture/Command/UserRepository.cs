0e67ab4d56564d5e18364c2eab436d85b507a5c8
Vhon Michael <vhontabalan@gmail.com>
2026-02-24 04:19:58 +0800

    feat: implementation of IserRepository

---
+++ HazelNet-Infrastracture/Command/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using HazelNet_Application.Interface;
+using HazelNet_Domain.Models;
+using HazelNet_Infrastracture.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace HazelNet_Infrastracture.Command;
+
+public class UserRepository :  IUserRepository
+{
+    public readonly ApplicationDbContext _context;
+
+    private UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> EmailExistAsync(string email)
+    {
+        return await _context.User.AnyAsync(c => c.EmailAddress == email);
+    }
+
+    public async Task RegisterUserAsync(User user)
+    {
+        _context.User.Add(user);
+        await _context.SaveChangesAsync();
+    }
+}
