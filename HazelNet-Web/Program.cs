f71b800f0d97a1fe1fe3eba273b8da1106031ff1
Vhon Michael <vhontabalan@gmail.com>
2026-02-24 04:22:15 +0800

    feat: Program.cs config for auth
    adding middlewares, cookie config, adding of DI services as scope, and some simple Minimal API config

--- HazelNet-Web/Program.cs
+++ HazelNet-Web/Program.cs
@@ -1,6 +1,10 @@
+using HazelNet_Application.Interface;
+using HazelNet_Infrastracture.Command;
 using MudBlazor.Services;
 using HazelNet_Web.Core;
 using HazelNet_Infrastracture.DBContext;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +19,20 @@ builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        opt.Cookie.Name = "HazelNet.Auth";
+        opt.LoginPath = "/Login";
+        opt.AccessDeniedPath = "/Forbidden";
+        opt.ExpireTimeSpan = TimeSpan.FromMinutes(45);
+    });
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,11 +44,21 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Form Post for login and auth validation
+//Implement later
+
+app.MapGet("/account/logout", async (HttpContext HttpContext) =>
+{
+    await HttpContext.SignOutAsync();
+    return Results.Redirect("/login");
+});
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
