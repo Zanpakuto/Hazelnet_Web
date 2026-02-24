e90e2fdf8cef9a90fd3dad53b91942628d12033b
Vhon Michael <vhontabalan@gmail.com>
2026-02-25 04:20:05 +0800

    feat: use minimal api for form post for auth
    Also added needed DI and a Httpclient

--- HazelNet-Web/Program.cs
+++ HazelNet-Web/Program.cs
@@ -1,16 +1,19 @@
+using HazelNet_Application.Auth;
 using HazelNet_Application.Interface;
 using HazelNet_Infrastracture.Command;
 using MudBlazor.Services;
 using HazelNet_Web.Core;
 using HazelNet_Infrastracture.DBContext;
+using HazelNet_Web.Features.Account;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>( option =>
-    option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    option.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
@@ -32,6 +35,17 @@ builder.Services.AddCascadingAuthenticationState();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<RegisterHandler>();
+builder.Services.AddScoped<LoginHandler>();
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddHttpClient("LocalApi", (sp, client) =>
+{
+    var navManager = sp.GetRequiredService<NavigationManager>();
+    client.BaseAddress = new Uri(navManager.BaseUri);
+});
+
 
 var app = builder.Build();
 
@@ -52,8 +66,38 @@ app.UseAuthorization();
 
 //Form Post for login and auth validation
 //Implement later
+app.MapPost("/register", async (
+    HttpContext httpContext,
+    RegisterHandler handler,
+    AccountSignUp.RegisterAccountForm model) =>
+{
+    
+   var command = new RegisterHandler.RegisterUserCommand(model.Username, model.Email, model.Password);
+   var result = await handler.Handle(command);
 
-app.MapGet("/account/logout", async (HttpContext HttpContext) =>
+   if (result.Success)
+       return Results.Ok(new { success = true });
+   else
+       return Results.BadRequest(new { error = "Email exists" });
+});
+
+app.MapPost("/login", async (
+    HttpContext httpContext,
+    LoginHandler handler,
+    Login.RegisterAccountForm model) =>
+{
+    
+    var command = new LoginHandler.LoginQuery(model.Email, model.Password);
+    var result = await handler.Handle(command);
+
+    if (result.Success)
+        return Results.Ok(new { success = true });
+    else
+        return Results.BadRequest(new { error = "Wrong credentials" });
+});
+
+
+app.MapGet("/logout", async (HttpContext HttpContext) =>
 {
     await HttpContext.SignOutAsync();
     return Results.Redirect("/login");
