d1e3f64f993cefd139e393a1f837a322c99a67f0
Vhon Michael <vhontabalan@gmail.com>
2026-04-13 04:54:20 +0800

    feat: create a command for creating new deck

---
+++ HazelNet-Application/CQRS/Features/Decks/Commands/CreateDeckCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using HazelNet_Application.CQRS.Abstractions;
+using HazelNet_Domain.IRepository;
+using HazelNet_Domain.Models;
+
+namespace HazelNet_Application.CQRS.Features.Decks.Commands;
+
+public class CreateDeckCommandHandler : ICommandHandler<CreateDeckCommand, int>
+{
+    private IDeckRepository _deckRepository;
+
+    public CreateDeckCommandHandler(IDeckRepository deckRepository)
+    {
+        _deckRepository = deckRepository;
+    }
+
+
+    public async Task<int> Handle(CreateDeckCommand command)
+    {
+        var deck = new Deck
+        {
+            DeckName = command.DeckName,
+            DeckDescription = command.DeckDescription,
+            CreationDate = DateTime.Now,
+            LastAcess = DateTime.Now,
+            UserId = command.UserId,
+        };
+        
+        await _deckRepository.CreateAsync(deck);
+        return deck.Id;
+    }
+}
