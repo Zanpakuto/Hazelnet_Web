d8685d48fcd0c2418e78e0e4df20c71f1a9317f2
Vhon Michael <vhontabalan@gmail.com>
2026-04-14 02:28:29 +0800

    feat: remove fake list and implement getting of decks from db

--- HazelNet-Application/CQRS/Features/Decks/Queries/GetDecksQueryHandler.cs
+++ HazelNet-Application/CQRS/Features/Decks/Queries/GetDecksQueryHandler.cs
@@ -1,5 +1,5 @@
 ﻿using HazelNet_Application.CQRS.Abstractions;
-
+using HazelNet_Domain.IRepository;
 using HazelNet_Domain.Models;
 using HazelNet_Web.ViewModel;
 
@@ -8,48 +8,16 @@ namespace HazelNet_Application.CQRS.Features.Decks.Queries;
 public class GetDecksQueryHandler 
     : IQueryHandler<GetDecksQuery, List<DeckViewModel>>
 {
-    public Task<List<DeckViewModel>> Handle(GetDecksQuery query)
+    private readonly IDeckRepository _deckRepository;
+    public GetDecksQueryHandler(IDeckRepository deckRepository)
     {
-        // fetch from DB later
-        var decks = new List<Deck>
-        {
-            new Deck
-            {
-                Id = 1,
-                DeckName = "System Architecture",
-                DeckDescription = "Core concepts of distributed systems and microservices.",
-                CreationDate = new DateTime(2026, 1, 10),
-                LastAcess = DateTime.Now.AddHours(-2),
-                UserId = 4
-            },
-            new Deck
-            {
-                Id = 2,
-                DeckName = "Rust Fundamentals",
-                DeckDescription = "Memory safety, borrowing, and lifetimes.",
-                CreationDate = new DateTime(2026, 3, 5),
-                LastAcess = DateTime.Now.AddDays(-1),
-                UserId = 4
-            },
-            new Deck
-            {
-                Id = 3,
-                DeckName = "Advanced .NET Features",
-                DeckDescription = null, 
-                CreationDate = new DateTime(2025, 11, 20),
-                LastAcess = DateTime.Now.AddDays(-15),
-                UserId = 4
-            },
-            new Deck
-            {
-                Id = 4,
-                DeckName = "LeetCode Problems",
-                DeckDescription = "A collection of leetcode problems and solution", 
-                CreationDate = new DateTime(2025, 11, 20),
-                LastAcess = DateTime.Now.AddDays(-25),
-                UserId = 4
-            }
-        };
+        _deckRepository = deckRepository;
+    }
+        
+    public async Task<List<DeckViewModel>> Handle(GetDecksQuery query)
+    {
+
+        var decks = await _deckRepository.GetDeckByUserIdAsync(query.UserId);
 
 
         var result = decks.Select(d => new DeckViewModel
@@ -60,7 +28,7 @@ public class GetDecksQueryHandler
             LastDateAccessed = d.LastAcess
         }).ToList();
 
-        return Task.FromResult(result);
+        return result;
     }
 }
 
